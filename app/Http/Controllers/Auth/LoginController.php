fbdce14da941df8d18c0ae168fb62ba777ac45a6
All4site <35457395+all4site@users.noreply.github.com>
2020-10-11 20:43:54 +0300

    Initial commit

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use App\Providers\RouteServiceProvider;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+use Laravel\Socialite\Facades\Socialite;
+
+class LoginController extends Controller
+{
+	/*
+	|--------------------------------------------------------------------------
+	| Login Controller
+	|--------------------------------------------------------------------------
+	|
+	| This controller handles authenticating users for the application and
+	| redirecting them to your home screen. The controller uses a trait
+	| to conveniently provide its functionality to your applications.
+	|
+	*/
+
+	use AuthenticatesUsers;
+
+	/**
+	 * Where to redirect users after login.
+	 *
+	 * @var string
+	 */
+	protected $redirectTo = RouteServiceProvider::HOME;
+
+	/**
+	 * Create a new controller instance.
+	 *
+	 * @return void
+	 */
+	public function __construct()
+	{
+		$this->middleware('guest')->except('logout');
+	}
+
+	public function redirectToProvider()
+	{
+		return Socialite::driver('github')->redirect();
+	}
+
+	/**
+	 * Obtain the user information from GitHub.
+	 *
+	 * @return string
+	 */
+	public function handleProviderCallback()
+	{
+		$user = Socialite::driver('github')->user();
+		$user = User::firstOrCreate([
+				'email' => $user->getEmail()
+		],[
+			'name' => $user->getName(),
+			'password' => Hash::make(Str::random(24))
+	]);
+
+		Auth::login($user, true);
+		return redirect('/');
+	}
+}
